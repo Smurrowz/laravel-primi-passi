368252ae66fe0c306c0d691b750970efe66962f3
Smurrowz <nigeldn1995@gmail.com>
2022-10-06 16:41:26 +0200

    first commit

---
+++ resources/views/homepage.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>homepage</title>
+</head>
+<body>
+  <h1>yo whad up</h1>
+  <span>{{$nome}}</span>
+  <span>{{$cognome}}</span>
+  <h4>{{$azione}}</h4>
+</body>
+</html>
